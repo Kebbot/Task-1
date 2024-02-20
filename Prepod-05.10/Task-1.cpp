012fb262b9aef3d08e45ea5181ba292234b5ebb7
Nikolz <126600682+Kebbot@users.noreply.github.com>
2024-02-20 22:18:56 +0300

    First Task

---
+++ Prepod-05.10/Task-1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	int seconds, minutes, hours;
+	cout << "Введите секунды: ";
+	cin >> seconds;
+	minutes = seconds / 60;
+	seconds = seconds % 60;
+	hours = minutes / 60;
+	minutes = minutes % 60;
+	cout << "Часов: " << hours;
+	cout << "\tМинут: " << minutes;
+	cout << "\t Секунд: " << seconds;
+
+
+
+	return 0;
+}
